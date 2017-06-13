b61a2b225f5aa01597e84b7669ef1b8725bf06b0
nesa.asoudeh <nesa.asoudeh@irdeto.com>
2017-06-13 16:20:48 -0400

    Test commit

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class dgame_vs extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! This is from Nesa :-)
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
